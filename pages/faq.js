4e549237c5197a704d758137fe558a7e8463b0fd
Lucas Dias <lf-dias@outlook.com>
2023-08-01 18:47:22 -0300

    Aula 01: conhecendo e instalando NextJS; entendendo rotas

---
+++ pages/faq.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+export default function FAQ(){
+  return(
+    <div>
+      <h1>Alura Cases - FAQ</h1>
+      <Link href="/">
+      Ir para Home
+      </Link>
+    </div>
+  )
+}
