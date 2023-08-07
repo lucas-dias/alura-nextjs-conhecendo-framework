20a60c4e3506c5292d8db5242503b4f7bf029bfa
Lucas Dias <lf-dias@outlook.com>
2023-08-07 17:26:13 -0300

    Aula 5: publicando o projeto na vercel

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,6 @@
-import Link from 'next/link'
-import Head from 'next/head'
+import FAQScreen from '../src/screens/FAQScreen';
+
+export default FAQScreen;
 
 export async function getStaticProps() {
   const API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -18,28 +19,28 @@ export async function getStaticProps() {
 }
 
 
-export default function FAQ({faq}){
-  return(
-    <div>
-      <Head>
-        <title>FAQ - Alura Cases</title>
-      </Head>
-      <h1>FAQ - Alura Cases</h1>
-      <Link href="/">
-      Ir para Home
-      </Link>
-      <ul>
-        {faq.map(({answer, question}) => (
-          <div>
-              <li key={question}>
-                <article>
-                  <h2>{question}</h2>
-                  <p>{answer}</p>
-                 </article>
-              </li>
-          </div>
-        ))}
-      </ul>
-    </div>
-  )
-}
+// export default function FAQ({faq}){
+//   return(
+//     <div>
+//       <Head>
+//         <title>FAQ - Alura Cases</title>
+//       </Head>
+//       <h1>FAQ - Alura Cases</h1>
+//       <Link href="/">
+//       Ir para Home
+//       </Link>
+//       <ul>
+//         {faq.map(({answer, question}) => (
+//           <div>
+//               <li key={question}>
+//                 <article>
+//                   <h2>{question}</h2>
+//                   <p>{answer}</p>
+//                  </article>
+//               </li>
+//           </div>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
